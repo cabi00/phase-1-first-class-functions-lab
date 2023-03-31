7f16b4dfd314716d0322fd1beb9c3611992914f4
vanessa garcia <venessag0908@gmail.com>
2023-03-31 16:25:29 -0600

    advanced functions

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {  
+    return drivers.slice(0,2);
+    }
+
+const returnLastTwoDrivers = function(drivers) {  
+     return drivers.slice(-2);
+}
+
+//const selectingDrivers  = [returnFirstTwoDrivers(), returnLastTwoDrivers()] 
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+const createFareMultiplier = function(multi){
+return function(){
+    return multi*multi;
+    }
+}
+const fareDoubler = function(multi){
+return multi*2;
+}
+const fareTripler = function(multi){
+    return multi*3;
+}
+const selectDifferentDrivers = function(drivers, funct){
+    return funct(drivers);
+    }
